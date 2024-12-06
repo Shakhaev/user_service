f7229222803e6bcb5ffd5c55bfef07ba2d59e41d
0Miha0 <140548440+0Miha0@users.noreply.github.com>
2024-12-06 20:12:10 +0500

    https://faang-school.atlassian.net/browse/BJS2-41816

--- src/main/java/school/faang/user_service/dto/user/UserDto.java
+++ src/main/java/school/faang/user_service/dto/user/UserDto.java
@@ -1,11 +1,19 @@
 package school.faang.user_service.dto.user;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import school.faang.user_service.entity.contact.PreferredContact;
+
+import java.time.LocalDateTime;
 
 @Builder
 @Getter
@@ -14,7 +22,23 @@ import lombok.Setter;
 @NoArgsConstructor
 @AllArgsConstructor
 public class UserDto {
+
     private Long id;
+
+    @NotBlank(message = "Username must not be blank")
+    @Size(max = 50, message = "Username must not exceed 50 characters")
     private String username;
+
+    @NotBlank(message = "Email must not be blank")
+    @Email(message = "Email must be a valid email address")
     private String email;
+
+    @Pattern(regexp = "^\\+?[0-9. ()-]{7,25}$", message = "Phone must be a valid number")
+    private String phone;
+
+    @NotNull(message = "Updated date must not be null")
+    private LocalDateTime updatedAt;
+
+    @NotNull(message = "Preferred contact must not be null")
+    private PreferredContact preference;
 }
