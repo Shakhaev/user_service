97f6ae011d2a979b03076f04630ca76dddc424c0
Doniyor Kurbanov <doniyor.kurbanov.24@gmail.com>
2024-12-16 01:23:57 +0500

    Feature - BJS2-41778
    With controller service and publisher
    
    (Version 2.0)

--- src/main/java/school/faang/user_service/publisher/GoalCompletedEventPublisher.java
+++ src/main/java/school/faang/user_service/publisher/GoalCompletedEventPublisher.java
@@ -3,6 +3,8 @@ package school.faang.user_service.publisher;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.retry.annotation.Backoff;
+import org.springframework.retry.annotation.Retryable;
 import org.springframework.stereotype.Component;
 import school.faang.user_service.config.redis.RedisProperties;
 import school.faang.user_service.event.GoalCompletedEvent;
@@ -15,6 +17,14 @@ public class GoalCompletedEventPublisher implements EventPublisher<GoalCompleted
     private final RedisProperties redisProperties;
 
     @Override
+    @Retryable(retryFor = Exception.class,
+            maxAttemptsExpression = "#{@retryProperties.maxAttempts}",
+            backoff = @Backoff(
+                    delayExpression = "#{@retryProperties.initialDelay}",
+                    multiplierExpression = "#{@retryProperties.multiplier}",
+                    maxDelayExpression = "#{@retryProperties.maxDelay}"
+            )
+    )
     public void publish(GoalCompletedEvent event) {
         redisTemplate.convertAndSend(redisProperties.getChannel().getGoalChannel(), event);
         log.info("Message sent to channel: {}", redisProperties.getChannel().getGoalChannel());
