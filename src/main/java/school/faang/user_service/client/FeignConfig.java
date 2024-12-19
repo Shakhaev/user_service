4c421766a31e14910c7bb513142888acc9516f1f
Ruslan G. <marlinco263@gmail.com>
2024-12-19 12:39:15 +0100

    BJS2-41896 completed

--- src/main/java/school/faang/user_service/client/FeignConfig.java
+++ src/main/java/school/faang/user_service/client/FeignConfig.java
@@ -3,7 +3,6 @@ package school.faang.user_service.client;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import school.faang.user_service.config.context.ContactPreferenceContext;
 import school.faang.user_service.config.context.UserContext;
 
 @Configuration
@@ -12,10 +11,5 @@ public class FeignConfig {
     @Bean
     public FeignUserInterceptor feignUserInterceptor(UserContext userContext) {
         return new FeignUserInterceptor(userContext);
-    }
-
-    @Bean
-    public FeignContactPreferenceInterceptor feignContactPreferenceInterceptor(ContactPreferenceContext contactPreferenceContext) {
-        return new FeignContactPreferenceInterceptor(contactPreferenceContext);
     }
 }
