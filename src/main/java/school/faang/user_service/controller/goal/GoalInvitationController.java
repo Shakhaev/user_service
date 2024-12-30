a1ce14a574a9532598df69e2cf222397e0b56d55
Ivan Baev <ibai86@yandex.ru>
2024-12-30 14:07:34 +0300

    add 2 classes

---
+++ src/main/java/school/faang/user_service/controller/goal/GoalInvitationController.java
@@ -0,0 +1,7 @@
+package school.faang.user_service.controller.goal;
+
+public class GoalInvitationController {
+    public void createInvitation(GoalInvitationDto invitation) {
+
+    }
+}
---
+++ src/main/java/school/faang/user_service/dto/goal/GoalInvitationDto.java
@@ -0,0 +1,11 @@
+package school.faang.user_service.dto.goal;
+
+import school.faang.user_service.entity.RequestStatus;
+
+public class GoalInvitationDto {
+    private Long id;
+    private Long inviterId;
+    private Long invitedUserId;
+    private Long goalId;
+    private RequestStatus status;
+}
