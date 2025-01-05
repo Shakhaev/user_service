dc243084b67700ee9bc8c2e3b180219c48fb32e3
ZhubanyshZh <zubanyszarylkasyn@gmail.com>
2025-01-05 16:37:58 +0500

    test(41281): BannerUserTest
    Introduced test cases to verify user banning logic, ensuring both user retrieval and ban status updates are correctly handled.

--- src/test/java/school/faang/user_service/service/user/BannerUserTest.java
+++ src/test/java/school/faang/user_service/service/user/BannerUserTest.java
@@ -7,12 +7,14 @@ import org.mockito.Captor;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import school.faang.user_service.entity.User;
 import school.faang.user_service.repository.UserRepository;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
@@ -37,5 +39,32 @@ public class BannerUserTest {
         bannerService.banUsers(List.of(1L));
 
         verify(userRepository, times(1)).findAllByIds(usersIdsCaptor.getValue());
+    }
+
+    @Test
+    public void testToBanUser_ShouldFoundUsersForBan() {
+        List<Long> usersIds = List.of(1L, 2L);
+        List<User> users = List.of(
+                User.builder().banned(false).id(1L).build(),
+                User.builder().banned(false).id(2L).build());
+        when(userRepository.findAllByIds(usersIdsCaptor.capture()))
+                .thenReturn(Optional.of(users));
+
+        bannerService.banUsers(usersIds);
+
+        verify(userRepository, times(1)).findAllByIds(usersIds);
+    }
+
+    @Test
+    public void testToSetBanToUsers_ShouldUpdateBanStatus() {
+        List<User> users = List.of(
+                User.builder().banned(false).id(1L).build(),
+                User.builder().banned(false).id(2L).build());
+
+        bannerService.setBanToUsers(users);
+
+        for(User user: users) {
+            assertTrue(user.isBanned());
+        }
     }
 }
