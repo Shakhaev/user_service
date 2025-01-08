28cf4f586560c36f9c03fcca6cb3e1ba0edcd04c
Ivan Baev <ibai86@yandex.ru>
2025-01-09 02:21:15 +0300

    add controller and mapper

--- src/main/java/school/faang/user_service/controller/goal/GoalInvitationController.java
+++ src/main/java/school/faang/user_service/controller/goal/GoalInvitationController.java
@@ -1,7 +1,20 @@
 package school.faang.user_service.controller.goal;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import school.faang.user_service.dto.goal.GoalInvitationDto;
+import school.faang.user_service.mapper.GoalInvitationMapper;
+import school.faang.user_service.service.GoalInvitationService;
+
+@RequiredArgsConstructor
+@Controller
 public class GoalInvitationController {
-    public void createInvitation(GoalInvitationDto invitation) {
+    private final GoalInvitationService goalInvitationService;
+    private final GoalInvitationMapper goalInvitationMapper;
+
+    public void createInvitation(GoalInvitationDto invitationDto) {
+
+        goalInvitationService.createInvitation(goalInvitationMapper.toEntity(invitationDto));
 
     }
 }
