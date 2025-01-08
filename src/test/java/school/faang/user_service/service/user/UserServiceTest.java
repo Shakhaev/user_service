13b66b5f6dbb2a5f0546af9db8b6386f225d45d8
Dmitry Shakhaev <dmtry.shahaev@gmail.com>
2025-01-08 18:47:53 +0300

    BJS2-41867 - Уведомление о подписке

--- src/test/java/school/faang/user_service/service/user/UserServiceTest.java
+++ src/test/java/school/faang/user_service/service/user/UserServiceTest.java
@@ -15,6 +15,8 @@ import org.springframework.mock.web.MockMultipartFile;
 import school.faang.user_service.config.context.UserContext;
 import school.faang.user_service.dto.user.Person;
 import school.faang.user_service.dto.user.UpdateUsersRankDto;
+import school.faang.user_service.entity.contact.ContactPreference;
+import school.faang.user_service.entity.contact.PreferredContact;
 import school.faang.user_service.entity.country.Country;
 import school.faang.user_service.entity.user.User;
 import school.faang.user_service.exception.data.DataValidationException;
@@ -78,6 +80,7 @@ public class UserServiceTest {
                 .id(3L)
                 .mentors(List.of(secondUser))
                 .mentees(List.of(firstUser))
+                .contactPreference(ContactPreference.builder().preference(PreferredContact.EMAIL).build())
                 .build();
         user = thirdUser;
         users = List.of(thirdUser);
