7e6681e3c6bb75c2e1d2ddfd9e8cad201884942a
Ivan Baev <ibai86@yandex.ru>
2025-01-10 00:59:29 +0300

    add first method

--- src/main/java/school/faang/user_service/dto/goal/GoalInvitationDto.java
+++ src/main/java/school/faang/user_service/dto/goal/GoalInvitationDto.java
@@ -1,7 +1,9 @@
 package school.faang.user_service.dto.goal;
 
+import lombok.Data;
 import school.faang.user_service.entity.RequestStatus;
 
+@Data
 public class GoalInvitationDto {
     private Long id;
     private Long inviterId;
--- src/main/java/school/faang/user_service/mapper/GoalInvitationMapper.java
+++ src/main/java/school/faang/user_service/mapper/GoalInvitationMapper.java
@@ -1,10 +1,12 @@
 package school.faang.user_service.mapper;
 
 import org.mapstruct.Mapper;
+import org.mapstruct.ReportingPolicy;
 import school.faang.user_service.dto.goal.GoalInvitationDto;
 import school.faang.user_service.entity.goal.GoalInvitation;
 
-@Mapper(componentModel = "spring")
+@Mapper(componentModel = "spring", unmappedSourcePolicy = ReportingPolicy.IGNORE)
 public interface GoalInvitationMapper {
     GoalInvitation toEntity(GoalInvitationDto dto);
+    GoalInvitationDto toDto(GoalInvitation entity);
 }
