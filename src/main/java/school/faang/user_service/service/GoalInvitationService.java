3ab5a2aabc944d1866822e52178ee444ab582aa3
Ivan Baev <ibai86@yandex.ru>
2025-01-10 00:59:29 +0300

    add first method

--- src/main/java/school/faang/user_service/service/GoalInvitationService.java
+++ src/main/java/school/faang/user_service/service/GoalInvitationService.java
@@ -1,16 +1,32 @@
 package school.faang.user_service.service;
 
+import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import school.faang.user_service.entity.goal.GoalInvitation;
+import school.faang.user_service.repository.UserRepository;
 import school.faang.user_service.repository.goal.GoalInvitationRepository;
+
+import java.util.Objects;
 
 @RequiredArgsConstructor
 @Service
 public class GoalInvitationService {
     private final GoalInvitationRepository goalInvitationRepository;
+    private final UserRepository userRepository;
 
-    public void createInvitation(GoalInvitation invitation) {
-        goalInvitationRepository.save(invitation);
+    public GoalInvitation createInvitation(@NonNull GoalInvitation invitation) {
+        if (Objects.equals(invitation.getInviter(), invitation.getInvited()))
+            throw new RuntimeException();
+
+        if (!userRepository.existsById(invitation.getInvited().getId())) {
+            throw new IllegalArgumentException("Invited user does not exist.");
+        }
+
+        if (!userRepository.existsById(invitation.getInviter().getId())) {
+            throw new IllegalArgumentException("Inviting user does not exist.");
+        }
+
+        return goalInvitationRepository.save(invitation);
     }
 }
