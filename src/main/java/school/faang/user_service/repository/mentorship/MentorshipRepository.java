63ad389c2de363d4771c3b22cf8fc3260926349e
AesaQ <ilyaovchinnikov2095@gmail.com>
2025-01-09 14:51:27 +0500

    task solution without tests

--- src/main/java/school/faang/user_service/repository/mentorship/MentorshipRepository.java
+++ src/main/java/school/faang/user_service/repository/mentorship/MentorshipRepository.java
@@ -1,7 +1,10 @@
 package school.faang.user_service.repository.mentorship;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import school.faang.user_service.entity.User;
+
+import java.util.List;
 
 public interface MentorshipRepository extends CrudRepository<User, Long> {
 }
