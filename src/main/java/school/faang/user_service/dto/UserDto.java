7fbdd9828f1b278730f9b68b23730243f65a3978
Anton Graf <ant0ngraf@yandex.ru>
2025-01-09 11:05:12 +0600

    Убирает final в полях dto

--- src/main/java/school/faang/user_service/dto/UserDto.java
+++ src/main/java/school/faang/user_service/dto/UserDto.java
@@ -2,11 +2,15 @@ package school.faang.user_service.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
 
 @AllArgsConstructor
 @EqualsAndHashCode
+@Getter
+@Setter
 public class UserDto {
-    private final Long id;
-    private final String username;
-    private final String email;
+    private Long id;
+    private String username;
+    private String email;
 }
