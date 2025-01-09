c167a4e5c8dcba7bea8f1863ed940e1cebed4c22
Mark Janovskij <markyanovskiy@gmail.com>
2025-01-09 13:43:00 +0100

    analysis+mark work 9/11/1 integration test

--- src/test/java/school/faang/user_service/controller/recommendation/RecommendationControllerIntegrationTest.java
+++ src/test/java/school/faang/user_service/controller/recommendation/RecommendationControllerIntegrationTest.java
@@ -21,9 +21,7 @@ import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.TimeUnit;
 
 import java.time.LocalDateTime;
-import java.util.concurrent.TimeUnit;
 
-import static org.awaitility.Awaitility.await;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest(classes = UserServiceApplication.class)
