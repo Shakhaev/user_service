711e349cffef25db47648d8ef455063799ae3579
Ivan Baev <ibai86@yandex.ru>
2025-01-11 18:52:59 +0300

    add first service

--- src/main/java/school/faang/user_service/controller/goal/GoalInvitationController.java
+++ src/main/java/school/faang/user_service/controller/goal/GoalInvitationController.java
@@ -3,6 +3,7 @@ package school.faang.user_service.controller.goal;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import school.faang.user_service.dto.goal.GoalInvitationDto;
+import school.faang.user_service.entity.goal.GoalInvitation;
 import school.faang.user_service.mapper.GoalInvitationMapper;
 import school.faang.user_service.service.GoalInvitationService;
 
@@ -12,9 +13,9 @@ public class GoalInvitationController {
     private final GoalInvitationService goalInvitationService;
     private final GoalInvitationMapper goalInvitationMapper;
 
-    public void createInvitation(GoalInvitationDto invitationDto) {
-
-        goalInvitationService.createInvitation(goalInvitationMapper.toEntity(invitationDto));
-
+    public GoalInvitationDto createInvitation(GoalInvitationDto dto) {
+        GoalInvitation entity = goalInvitationMapper.toEntity(dto);
+        GoalInvitation result = goalInvitationService.createInvitation(entity);
+        return goalInvitationMapper.toDto(result);
     }
 }
--- src/main/java/school/faang/user_service/dto/goal/GoalInvitationDto.java
+++ src/main/java/school/faang/user_service/dto/goal/GoalInvitationDto.java
@@ -1,13 +1,35 @@
 package school.faang.user_service.dto.goal;
 
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import school.faang.user_service.entity.RequestStatus;
 
+@AllArgsConstructor
+@NoArgsConstructor
 @Data
 public class GoalInvitationDto {
+    private static final String NOT_BE_NULL = "ID mustn't be null";
+    private static final String MUST_BE_POSITIVE = "ID must be a positive number";
+
+    @NotNull(message = NOT_BE_NULL)
+    @Positive(message = MUST_BE_POSITIVE)
     private Long id;
+
+    @NotNull(message = NOT_BE_NULL)
+    @Positive(message = MUST_BE_POSITIVE)
     private Long inviterId;
+
+    @NotNull(message = NOT_BE_NULL)
+    @Positive(message = MUST_BE_POSITIVE)
     private Long invitedUserId;
+
+    @NotNull(message = NOT_BE_NULL)
+    @Positive(message = MUST_BE_POSITIVE)
     private Long goalId;
+
+    @NotNull(message = NOT_BE_NULL)
     private RequestStatus status;
 }
