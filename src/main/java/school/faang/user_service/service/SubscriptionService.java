9885a1ebabe8e3f04fc64575f41c5dd9591e3452
Alexey Gorokhov <almigor@mail.ru>
2025-01-11 10:49:03 +0300

    Рефакторинг фильтров

--- src/main/java/school/faang/user_service/service/SubscriptionService.java
+++ src/main/java/school/faang/user_service/service/SubscriptionService.java
@@ -7,15 +7,15 @@ import school.faang.user_service.dto.SubscriptionUserFilterDto;
 import java.util.List;
 
 public interface SubscriptionService {
-    public void followUser(long followerId, long followeeId);
+    void followUser(long followerId, long followeeId);
 
-    public void unfollowUser(long followerId, long followeeId);
+    void unfollowUser(long followerId, long followeeId);
 
-    public List<SubscriptionUserDto> getFollowers(long followeeId, SubscriptionUserFilterDto filter);
+    List<SubscriptionUserDto> getFollowers(long followeeId, SubscriptionUserFilterDto filter);
 
-    public List<SubscriptionUserDto> getFollowing(long followeeId, SubscriptionUserFilterDto filter);
+    List<SubscriptionUserDto> getFollowing(long followeeId, SubscriptionUserFilterDto filter);
 
-    public RecordsQuantityDto getFollowersCount(long followeeId);
+    RecordsQuantityDto getFollowersCount(long followeeId);
 
-    public RecordsQuantityDto getFollowingCount(long followerId);
+    RecordsQuantityDto getFollowingCount(long followerId);
 }
