48a23bb332298fffb1a212c88d4e422bea44ac14
Anton Graf <ant0ngraf@yandex.ru>
2025-01-12 22:15:20 +0600

    Добавляет валидацию полей в UserDto

--- src/main/java/school/faang/user_service/dto/UserDto.java
+++ src/main/java/school/faang/user_service/dto/UserDto.java
@@ -1,14 +1,26 @@
 package school.faang.user_service.dto;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Null;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
+import school.faang.user_service.dto.transfer.Exist;
+import school.faang.user_service.dto.transfer.New;
 
 @AllArgsConstructor
 @Getter
 @Setter
 public class UserDto {
+    @NotNull(groups = {Exist.class})
+    @Null(groups = {New.class})
     private Long id;
+
+    @NotNull(groups = {New.class})
     private String username;
+
+    @NotNull(groups = {New.class})
+    @Email(groups = {New.class})
     private String email;
 }
