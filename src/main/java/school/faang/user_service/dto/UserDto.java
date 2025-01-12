636e70020ba3eca6d67c86708f9ae63039a9d342
Anton Graf <ant0ngraf@yandex.ru>
2025-01-12 22:19:25 +0600

    Добавляет валидацию полей UserDto для выходных данных

--- src/main/java/school/faang/user_service/dto/UserDto.java
+++ src/main/java/school/faang/user_service/dto/UserDto.java
@@ -1,11 +1,13 @@
 package school.faang.user_service.dto;
 
+import com.fasterxml.jackson.annotation.JsonView;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Null;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
+import school.faang.user_service.dto.transfer.Details;
 import school.faang.user_service.dto.transfer.Exist;
 import school.faang.user_service.dto.transfer.New;
 
@@ -15,12 +17,15 @@ import school.faang.user_service.dto.transfer.New;
 public class UserDto {
     @NotNull(groups = {Exist.class})
     @Null(groups = {New.class})
+    @JsonView({Details.class})
     private Long id;
 
     @NotNull(groups = {New.class})
+    @JsonView({Details.class})
     private String username;
 
     @NotNull(groups = {New.class})
     @Email(groups = {New.class})
+    @JsonView({Details.class})
     private String email;
 }
