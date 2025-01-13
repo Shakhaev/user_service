318f2a43cee399c2c2173e9c76c6c88437ab2052
Ivan Baev <ibai86@yandex.ru>
2025-01-14 02:54:59 +0300

    add filter interface and one filter

--- src/main/java/school/faang/user_service/UserServiceApplication.java
+++ src/main/java/school/faang/user_service/UserServiceApplication.java
@@ -2,11 +2,13 @@ package school.faang.user_service;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
-
+import school.faang.user_service.dto.goal.GoalInvitationDto;
+@Slf4j
 @SpringBootApplication
 @EnableFeignClients("school.faang.user_service.client")
 public class UserServiceApplication {
