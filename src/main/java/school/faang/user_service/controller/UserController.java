4816e98ea6edbfd53e1d6ccef8f8c2d71d0bbcaf
romenskd <romenskyid@gmail.com>
2025-01-15 00:49:58 +0100

    рефакторинг контроллера

--- src/main/java/school/faang/user_service/controller/UserController.java
+++ src/main/java/school/faang/user_service/controller/UserController.java
@@ -2,7 +2,6 @@ package school.faang.user_service.controller;
 
 import jakarta.validation.constraints.NotNull;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,8 +14,7 @@ public class UserController {
     private final UserService userService;
 
     @PostMapping
-    public String deactivateUser(@RequestParam @NotNull Long userId) {
+    public void deactivateUser(@RequestParam @NotNull Long userId) {
         userService.deactivateUser(userId);
-        return "Пользователь успешно деактивирован";
     }
 }
