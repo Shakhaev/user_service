73667145fc85f66aaaa3a0416a95ae2bf0dba64d
tatgricenko <tattgricenko@gmail.com>
2025-01-14 22:45:27 +0300

    create recommendation 2

--- src/main/java/school/faang/user_service/controller/RecommendationController.java
+++ src/main/java/school/faang/user_service/controller/RecommendationController.java
@@ -6,6 +6,8 @@ import org.springframework.stereotype.Component;
 import school.faang.user_service.dto.recommendation.RecommendationDto;
 import school.faang.user_service.service.RecommendationService;
 
+import java.util.Optional;
+
 @Component
 @RequiredArgsConstructor
 public class RecommendationController {
@@ -13,13 +15,14 @@ public class RecommendationController {
 
     public RecommendationDto giveRecommendation(RecommendationDto recommendation) {
         if (validateRecommendation(recommendation)) {
-            return recommendationService.create(recommendation);
+            Optional<RecommendationDto> recommendationOptional = recommendationService.create(recommendation);
+
         } else {
             throw new DataValidationException("Recommendation is empty");
         }
     }
 
-    private boolean validateRecommendation(RecommendationDto recommendation) {
+    private boolean validateRecommendation(RecommendationDto recommendation) { // TODO: вынести валидацию в сервис?
         return !recommendation.getContent().isEmpty();
     }
 }
--- src/main/java/school/faang/user_service/service/RecommendationService.java
+++ src/main/java/school/faang/user_service/service/RecommendationService.java
@@ -5,12 +5,15 @@ import org.junit.jupiter.params.shadow.com.univocity.parsers.common.DataValidati
 import org.springframework.stereotype.Component;
 import school.faang.user_service.dto.recommendation.RecommendationDto;
 import school.faang.user_service.entity.recommendation.Recommendation;
+import school.faang.user_service.entity.recommendation.SkillOffer;
 import school.faang.user_service.mapper.recommendation.RecommendationMapper;
+import school.faang.user_service.repository.SkillRepository;
 import school.faang.user_service.repository.recommendation.RecommendationRepository;
 import school.faang.user_service.repository.recommendation.SkillOfferRepository;
 
 import java.time.LocalDateTime;
 import java.time.Period;
+import java.util.List;
 import java.util.Optional;
 
 import static java.time.LocalDateTime.*;
@@ -20,6 +23,7 @@ import static java.time.LocalDateTime.*;
 public class RecommendationService {
     private final RecommendationRepository recommendationRepository;
     private final SkillOfferRepository skillOfferRepository;
+    private final SkillRepository skillRepository;
     private final RecommendationMapper recommendationMapper;
 
     public Optional<RecommendationDto> create(RecommendationDto recommendationDto) {
@@ -33,12 +37,25 @@ public class RecommendationService {
             Period timeDifference = Period.between(lastRecommendation.getCreatedAt().toLocalDate(), now().toLocalDate());
 
             if (timeDifference.getMonths() < 6) {
-                throw new DataValidationException("The author can make a recommendationDto to the user no earlier than 6 months after the last recommendationDto. The last recommendationDto was given " + lastRecommendation.getCreatedAt());
+                throw new DataValidationException("The author can make a recommendation to the user no earlier than 6 months after the last recommendationDto. The last recommendationDto was given " + lastRecommendation.getCreatedAt());
                 return Optional.empty();
             }
         }
 
-        // проверка, что навыки, предлагаемые в рекомендации, существуют
+        // проверка, что навыки, предлагаемые в рекомендации, существуют TODO: сделать через StreamAPI
+        List<SkillOffer> skillOffers = recommendation.getSkillOffers();
+        for (SkillOffer skillOffer : skillOffers) {
+            if (!skillRepository.existsByTitle(skillOffer.getSkill().getTitle())) {
+                throw new DataValidationException("Skill " + skillOffer.getSkill().getTitle() + " doesn't exist");
+                return Optional.empty();
+            }
+        }
+
+        for (SkillOffer skillOffer : skillOffers) {
+            skillOfferRepository.create(skillOffer.getSkill().getId(), skillOffer.getRecommendation().getId());
+            // если у пользователя уже есть такой скилл, то добавить автора рекомендации гарантом к скиллу, если он ещё не стоит там
+        }
+
 
 
     }
