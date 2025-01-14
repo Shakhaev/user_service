80e49021b653ad4c9297870a72bd76f4b59a842d
therealadik <vladerm2000@yandex.ru>
2025-01-14 11:53:24 +0500

    fix

--- src/test/java/school/faang/user_service/service/SkillServiceTest.java
+++ src/test/java/school/faang/user_service/service/SkillServiceTest.java
@@ -32,7 +32,6 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
-import static org.junit.jupiter.api.Assertions.fail;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.Mockito.never;
@@ -78,7 +77,6 @@ class SkillServiceTest {
         CreateSkillDto createSkillDto = new CreateSkillDto("Java");
         Skill skillEntity = Skill.builder().id(1L).title("Java").build();
         SkillDto expectedSkillDto = new SkillDto(1L, "Java");
-        fail();
 
         when(skillRepository.existsByTitle("Java")).thenReturn(false);
         when(skillRepository.save(skillEntity)).thenReturn(skillEntity);
