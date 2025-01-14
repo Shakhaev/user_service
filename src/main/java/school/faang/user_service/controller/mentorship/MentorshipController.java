94d2e175c5429345f648447c630716a7472ce43f
AesaQ <ilyaovchinnikov2095@gmail.com>
2025-01-14 18:03:17 +0500

    Правки

--- src/main/java/school/faang/user_service/controller/mentorship/MentorshipController.java
+++ src/main/java/school/faang/user_service/controller/mentorship/MentorshipController.java
@@ -1,34 +1,27 @@
 package school.faang.user_service.controller.mentorship;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.*;
 import school.faang.user_service.dto.UserDto;
-import school.faang.user_service.service.mentorship.MentorshipService;
+import school.faang.user_service.service.MentorshipService;
 
 import java.util.List;
 
-@RestController
-@RequestMapping("/api/mentorship/")
 @RequiredArgsConstructor
 public class MentorshipController {
     private final MentorshipService mentorshipService;
 
-    @GetMapping("/mentees")
     public List<UserDto> getMentees(long id) {
         return mentorshipService.getMentees(id);
     }
 
-    @GetMapping("/mentors")
     public List<UserDto> getMentors(long id) {
         return mentorshipService.getMentors(id);
     }
 
-    @PostMapping("/remove-mentee/{mentorId}/{menteeId}")
-    public void deleteMentee(@PathVariable long mentorId, @PathVariable long menteeId) {
+    public void deleteMentee(long mentorId, long menteeId) {
         mentorshipService.deleteMentee(mentorId, menteeId);
     }
 
-    @PostMapping("/remove-mentor/{menteeId}/{mentorId}")
     public void deleteMentor(long menteeId, long mentorId) {
         mentorshipService.deleteMentor(menteeId, mentorId);
     }
