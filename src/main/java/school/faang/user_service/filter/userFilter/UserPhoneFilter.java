d4b1a8c5f56a34f9c27418adc1f52eec9f2cb518
Anton Graf <ant0ngraf@yandex.ru>
2025-01-14 11:33:04 +0600

    Добавляет тест фильтра UserPhoneFilter

--- src/main/java/school/faang/user_service/filter/userFilter/UserPhoneFilter.java
+++ src/main/java/school/faang/user_service/filter/userFilter/UserPhoneFilter.java
@@ -15,6 +15,10 @@ public class UserPhoneFilter implements UserFilter {
 
     @Override
     public Stream<User> apply(Stream<User> users, UserFilterDto filters) {
+        if (users == null || filters == null || filters.getPhonePattern() == null) {
+            return Stream.empty();
+        }
+
         return users.filter(user -> user.getPhone() != null && user.getPhone()
                 .toUpperCase().contains(filters.getPhonePattern().toUpperCase()));
     }
---
+++ src/test/java/school/faang/user_service/filter/userFilter/UserPhoneFilterTest.java
@@ -0,0 +1,90 @@
+package school.faang.user_service.filter.userFilter;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import school.faang.user_service.dto.UserFilterDto;
+import school.faang.user_service.entity.User;
+
+import java.util.List;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
+class UserPhoneFilterTest {
+    UserFilter userFilter = new UserPhoneFilter();
+
+    @Test
+    void isApplicable() {
+        UserFilterDto userFilterDto = UserFilterDto.builder()
+                .phonePattern("+795")
+                .build();
+
+        boolean actual = userFilter.isApplicable(userFilterDto);
+
+        Assertions.assertTrue(actual);
+    }
+
+    @Test
+    void isApplicablePhoneFilterIsNull() {
+        UserFilterDto userFilterDto = new UserFilterDto();
+        boolean actual = userFilter.isApplicable(userFilterDto);
+
+        Assertions.assertFalse(actual);
+    }
+
+    @Test
+    void isApplicableUserFilterDtoIsNull() {
+        boolean actual = userFilter.isApplicable(null);
+
+        Assertions.assertFalse(actual);
+    }
+
+    @Test
+    void apply() {
+        Stream<User> users = IntStream.range(0, 11)
+                .boxed()
+                .map(i -> User.builder()
+                        .phone("+795214%d".formatted(i))
+                        .build());
+
+        UserFilterDto userFilterDto = UserFilterDto.builder().phonePattern("+7952141").build();
+        List<User> except = List.of(
+                User.builder().phone("+7952141").build(),
+                User.builder().phone("+79521410").build()
+        );
+        List<User> actual = userFilter.apply(users, userFilterDto)
+                .toList();
+
+        Assertions.assertEquals(except, actual);
+    }
+
+    @Test
+    void applyPhoneFilterIsNull() {
+        Stream<User> users = IntStream.range(0, 10)
+                .boxed()
+                .map(i -> User.builder()
+                        .phone("+795214%d".formatted(i))
+                        .build());
+
+        UserFilterDto userFilterDto = UserFilterDto.builder().build();
+        List<User> except = List.of();
+        List<User> actual = userFilter.apply(users, userFilterDto)
+                .toList();
+
+        Assertions.assertEquals(except, actual);
+    }
+
+    @Test
+    void applyFilterDtoIsNull() {
+        Stream<User> users = IntStream.range(0, 10)
+                .boxed()
+                .map(i -> User.builder()
+                        .phone("+795214%d".formatted(i))
+                        .build());
+
+        List<User> except = List.of();
+        List<User> actual = userFilter.apply(users, null)
+                .toList();
+
+        Assertions.assertEquals(except, actual);
+    }
+}
