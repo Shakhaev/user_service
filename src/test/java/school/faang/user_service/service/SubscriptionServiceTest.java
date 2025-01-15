b9c83c021621170367bd4c0d4a930c807c20a617
Alexey Gorokhov <almigor@mail.ru>
2025-01-16 01:06:59 +0300

    тесты на getfolow*rs

--- src/test/java/school/faang/user_service/service/SubscriptionServiceTest.java
+++ src/test/java/school/faang/user_service/service/SubscriptionServiceTest.java
@@ -1,45 +1,67 @@
 package school.faang.user_service.service;
 
 import org.junit.Assert;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
+import school.faang.user_service.dto.SubscriptionUserDto;
+import school.faang.user_service.dto.SubscriptionUserFilterDto;
+import school.faang.user_service.entity.User;
 import school.faang.user_service.exception.DataValidationException;
+import school.faang.user_service.mapper.SubscriptionUserMapper;
 import school.faang.user_service.repository.SubscriptionRepository;
 import school.faang.user_service.service.impl.SubscriptionServiceImpl;
 
+import java.util.List;
+
+import static org.mockito.ArgumentMatchers.any;
+
 @ExtendWith(MockitoExtension.class)
 public class SubscriptionServiceTest {
-    @Mock
-    private SubscriptionRepository subscriptionRepository;
-    @InjectMocks
-    private SubscriptionServiceImpl subscriptionService;
+    //@Mock
+    //private SubscriptionRepository subscriptionRepository;
+    //private SubscriptionRepository subscriptionRepository;
+    //@InjectMocks
+    //private SubscriptionServiceImpl subscriptionService;
+    private SubscriptionRepository subscriptionRepositoryMock;
+    private SubscriptionService subscriptionService;
+    private SubscriptionFilter filterMock;
     private long followerId;
     private long followeeId;
+    private List<User> allUsers;
 
     @BeforeEach
     public void init() {
         followerId = 1L;
         followeeId = 2L;
+        allUsers = TestData.getUsers();
+
+        subscriptionRepositoryMock = Mockito.mock(SubscriptionRepository.class);
+        SubscriptionUserMapper mapperMock = Mockito.mock(SubscriptionUserMapper.class);
+        filterMock = Mockito.mock(SubscriptionFilter.class);
+        //SubscriptionFilter filter = new SubscriptionUserPageFilter();
+
+        List<SubscriptionFilter> filters = List.of(filterMock);
+        subscriptionService = new SubscriptionServiceImpl(subscriptionRepositoryMock, filters, mapperMock);
+
     }
 
     @Test
     @DisplayName("Follow To Another User")
     void testFollowOneUserByAnotherUser() {
         subscriptionService.followUser(followerId, followeeId);
-        Mockito.verify(subscriptionRepository, Mockito.times(1))
+        Mockito.verify(subscriptionRepositoryMock, Mockito.times(1))
                 .followUser(followerId, followeeId);
     }
 
     @Test
     @DisplayName("Follow By Himself")
     void testFollowUserByHimself() {
-        Mockito.when(subscriptionRepository.existsByFollowerIdAndFolloweeId(followerId, followerId))
+        Mockito.when(subscriptionRepositoryMock.existsByFollowerIdAndFolloweeId(followerId, followerId))
                 .thenThrow(new DataValidationException("!!"));
         Assert.assertThrows(DataValidationException.class, () -> subscriptionService.followUser(followerId, followerId));
     }
@@ -48,7 +70,7 @@ public class SubscriptionServiceTest {
     @DisplayName("Unfollow Another User")
     void testUnfollowOneUserFromAnotherUser() {
         subscriptionService.unfollowUser(followerId, followeeId);
-        Mockito.verify(subscriptionRepository, Mockito.times(1))
+        Mockito.verify(subscriptionRepositoryMock, Mockito.times(1))
                 .unfollowUser(followerId, followeeId);
     }
 
@@ -56,7 +78,7 @@ public class SubscriptionServiceTest {
     @DisplayName("Get Followers Count")
     void testGetFollowersCount() {
         subscriptionService.getFollowersCount(followeeId);
-        Mockito.verify(subscriptionRepository, Mockito.times(1))
+        Mockito.verify(subscriptionRepositoryMock, Mockito.times(1))
                 .findFollowersAmountByFolloweeId(followeeId);
     }
 
@@ -64,8 +86,43 @@ public class SubscriptionServiceTest {
     @DisplayName("Get Following Count")
     void testGetFollowingCount() {
         subscriptionService.getFollowingCount(followerId);
-        Mockito.verify(subscriptionRepository, Mockito.times(1))
+        Mockito.verify(subscriptionRepositoryMock, Mockito.times(1))
                 .findFolloweesAmountByFollowerId(followerId);
     }
 
+    @Test
+    @DisplayName("Get Followers")
+    void testGetFollowers() {
+        SubscriptionUserFilterDto subscriptionUserFilterDto = SubscriptionUserFilterDto.builder()
+                .page(1)
+                .pageSize(10)
+                .build();
+
+        Mockito.when(subscriptionRepositoryMock.findByFolloweeId(followeeId)).thenReturn(allUsers.stream());
+        Mockito.when(filterMock.isApplicable(subscriptionUserFilterDto)).thenReturn(true);
+        Mockito.when(filterMock.apply(any(), any())).thenReturn(allUsers.stream());
+
+        List<SubscriptionUserDto> actualUsersDtos =
+                subscriptionService.getFollowers(followeeId, subscriptionUserFilterDto);
+
+        Assertions.assertEquals(3,actualUsersDtos.size());
+     }
+
+    @Test
+    @DisplayName("Get Followees")
+    void testGetFollowees() {
+        SubscriptionUserFilterDto subscriptionUserFilterDto = SubscriptionUserFilterDto.builder()
+                .page(1)
+                .pageSize(10)
+                .build();
+
+        Mockito.when(subscriptionRepositoryMock.findByFolloweeId(followerId)).thenReturn(allUsers.stream());
+        Mockito.when(filterMock.isApplicable(subscriptionUserFilterDto)).thenReturn(true);
+        Mockito.when(filterMock.apply(any(), any())).thenReturn(allUsers.stream());
+
+        List<SubscriptionUserDto> actualUsersDtos =
+                subscriptionService.getFollowing(followerId, subscriptionUserFilterDto);
+
+        Assertions.assertEquals(3,actualUsersDtos.size());
+    }
 }
