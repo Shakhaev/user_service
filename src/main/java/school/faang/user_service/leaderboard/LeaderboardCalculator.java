c3b0e8a4e9c28e2d21857445aa2b1a30b18487bf
rostex <rostex2@yandex.ru>
2025-01-16 21:18:39 +0300

    Рефакторинг

--- src/main/java/school/faang/user_service/leaderboard/LeaderboardCalculator.java
+++
@@ -1,9 +0,0 @@
-package school.faang.user_service.leaderboard;
-
-import jakarta.validation.constraints.NotNull;
-import school.faang.user_service.dto.user.LeaderboardDto;
-
-public interface LeaderboardCalculator {
-
-    int getScore(@NotNull LeaderboardDto userDto);
-}
