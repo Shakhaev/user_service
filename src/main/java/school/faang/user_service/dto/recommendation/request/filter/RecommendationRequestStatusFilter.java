7339004103d625f0a7d8cdd79e6272931b03c65c
Pavel <45390423+rsticks@users.noreply.github.com>
2025-01-16 13:32:49 +0300

    BJS2-52597 Запрос рекомендации refactoring

--- src/main/java/school/faang/user_service/dto/recommendation/request/filter/RecommendationRequestStatusFilter.java
+++ src/main/java/school/faang/user_service/dto/recommendation/request/filter/RecommendationRequestStatusFilter.java
@@ -7,19 +7,18 @@ import java.util.List;
 import java.util.Objects;
 
 @Component
-public class RecommendationRequestMessageAndStatusFilter implements RecommendationRequestFilter {
+public class RecommendationRequestStatusFilter implements RecommendationRequestFilter {
 
     @Override
     public boolean isApplicable(RecommendationRequestFilterDto requestFilterDto) {
         return requestFilterDto != null
-                && requestFilterDto.getStatus() != null
-                && requestFilterDto.getMessage() != null;
+                && requestFilterDto.getStatus() != null;
     }
 
     @Override
     public List<RecommendationRequest> apply(List<RecommendationRequest> recommendationRequests, RecommendationRequestFilterDto requestFilterDto) {
         return recommendationRequests.stream()
-                .filter(request -> Objects.equals(request.getStatus().name(), requestFilterDto.getStatus())
-                        && Objects.equals(request.getMessage(), requestFilterDto.getMessage())).toList();
+                .filter(request -> Objects.equals(request.getStatus().name(), requestFilterDto.getStatus()))
+                .toList();
     }
 }
