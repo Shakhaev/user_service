c3316ef692e446a029cc1bd4b6399de1bbbe0f2b
az3l1t <eternity_cr9p@mail.ru>
2025-01-16 16:50:49 +0300

    kafka configuration

--- src/main/java/school/faang/user_service/controller/RatingController.java
+++ src/main/java/school/faang/user_service/controller/RatingController.java
@@ -3,7 +3,6 @@ package school.faang.user_service.controller;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 import school.faang.user_service.dto.rating.LeaderTableDto;
-import school.faang.user_service.dto.rating.RatingDto;
 import school.faang.user_service.dto.rating.UserComparingDto;
 import school.faang.user_service.service.RatingService;
 
@@ -14,15 +13,6 @@ import java.util.List;
 @RequiredArgsConstructor
 public class RatingController {
     private final RatingService ratingService;
-
-    /*
-        For kafka integration
-        (пока бессмысленно добавлять брокер, потому что у нас 0 реализаций в других сервисах)
-     */
-    @PostMapping
-    public void addRatingToUser(@RequestBody RatingDto ratingDTO) {
-        ratingService.addPoints(ratingDTO);
-    }
 
     @GetMapping("/leaders/{limit}")
     public List<LeaderTableDto> getTableLeaders(@PathVariable int limit, @RequestBody UserComparingDto userComparingDto) {
