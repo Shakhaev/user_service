c5df732a74954d5c56b774f2512d2838eb6172dc
Anton Graf <ant0ngraf@yandex.ru>
2025-01-16 13:14:45 +0600

    Добавляет импорт Optional

--- src/test/java/school/faang/user_service/service/goal/GoalServiceTest.java
+++ src/test/java/school/faang/user_service/service/goal/GoalServiceTest.java
@@ -23,6 +23,7 @@ import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.NoSuchElementException;
+import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
