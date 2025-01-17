5c13fcb5261a4d60fc37f6bbe34b6368a7946bfe
therealadik <vladerm2000@yandex.ru>
2025-01-17 10:18:24 +0500

    update kafka

--- src/main/java/school/faang/user_service/config/KafkaTopics.java
+++ src/main/java/school/faang/user_service/config/KafkaTopics.java
@@ -1,5 +1,8 @@
 package school.faang.user_service.config;
 
+import lombok.experimental.UtilityClass;
+
+@UtilityClass
 public class KafkaTopics {
     public final static String PROMOTION_BOUGHT_TOPIC = "promotion_bought";
     public final static String EVENT_KEY = "event";
