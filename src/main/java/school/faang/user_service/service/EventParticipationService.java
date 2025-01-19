760ff247cd4a80cdc9efc6825c22db1cb6bf8c38
sanctusgermanus72362 <sanctus-germanus7@yandex.ru>
2025-01-19 11:16:40 +0300

    Классы и методы "Участия в событии"

--- src/main/java/school/faang/user_service/service/EventParticipationService.java
+++ src/main/java/school/faang/user_service/service/EventParticipationService.java
@@ -4,46 +4,43 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 import school.faang.user_service.entity.User;
-import school.faang.user_service.mapper.EventMapper;
-import school.faang.user_service.mapper.UserMapper;
 import school.faang.user_service.repository.event.EventParticipationRepository;
 
 import java.util.List;
-import java.util.stream.Collectors;
-
 
 @Slf4j
 @Component
 @RequiredArgsConstructor
 
 public class EventParticipationService {
-    private final UserMapper userMapper;
-    private final EventMapper eventMapper;
     private final EventParticipationRepository repository;
 
     public void registerParticipant(long eventId, long userId) {
 
-        if (getUserIdFromList(eventId).contains(userId))
+        if (isUserRegisteredOnEvent(eventId))
             throw new IllegalArgumentException("User already registered for this event");
         else repository.register(eventId, userId);
     }
 
     public void unregisterParticipant(long eventId, long userId) {
-        if (!getUserIdFromList(eventId).contains(userId))
+        if (!isUserRegisteredOnEvent(eventId))
             throw new IllegalArgumentException("User not registered for this event");
         else repository.unregister(eventId, userId);
     }
 
-    public void getParticipant(long eventId) {
-        repository.findAllParticipantsByEventId(eventId);
+    public List<User> getParticipant(long eventId) {
+        return repository.findAllParticipantsByEventId(eventId);
     }
 
-    public void getParticipantsCount(long eventId) {
-        repository.countParticipants(eventId);
+    public int getParticipantsCount(long eventId) {
+        return repository.countParticipants(eventId);
     }
 
-    private List<Long> getUserIdFromList(long eventId) {
-        return repository.findAllParticipantsByEventId(eventId).stream().map(User::getId).collect(Collectors.toList());
+    private boolean isUserRegisteredOnEvent(long eventId) {
+        List<Long> getListOfUserId = repository.findAllParticipantsByEventId(eventId).stream()
+                .map(User::getId).toList();
+        return getListOfUserId.contains(eventId);
+
     }
 
 }
