3a6334d022c9559e3c16645d2c3aa3f4184f1296
Sultan <sultan.inked@gmail.com>
2025-01-19 22:18:24 +0500

    configure kafka template task perform

---
+++ src/main/java/school/faang/user_service/dto/user/message/ProfileViewMessage.java
@@ -0,0 +1,16 @@
+package school.faang.user_service.dto.user.message;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Builder
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class ProfileViewMessage {
+    private String message;
+}
