152c90194abd0911c59e5764e5da0107d23afe41
AesaQ <ilyaovchinnikov2095@gmail.com>
2025-01-19 14:56:09 +0500

    task solution

---
+++ src/main/java/school/faang/user_service/service/GoalService.java
@@ -0,0 +1,28 @@
+package school.faang.user_service.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import school.faang.user_service.entity.User;
+import school.faang.user_service.entity.goal.Goal;
+import school.faang.user_service.repository.goal.GoalRepository;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class GoalService {
+    private final GoalRepository repository;
+
+    public void removeUserFromGoal(Goal goal, long userId) {
+        List<User> users = goal.getUsers();
+        if (!users.removeIf(user -> user.getId() == userId)) {
+            throw new IllegalArgumentException("Пользователь у цели с айди "+ userId +" не был найден");
+        }
+        goal.setUsers(users);
+        if (goal.getUsers().size() < 1) {
+            repository.deleteById(goal.getId());
+        } else {
+            repository.save(goal);
+        }
+    }
+}
--- src/main/java/school/faang/user_service/service/UserService.java
+++ src/main/java/school/faang/user_service/service/UserService.java
@@ -3,14 +3,42 @@ package school.faang.user_service.service;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import school.faang.user_service.entity.User;
+import school.faang.user_service.entity.goal.Goal;
 import school.faang.user_service.exception.ResourceNotFoundException;
 import school.faang.user_service.repository.UserRepository;
+import school.faang.user_service.service.goal.GoalService;
+
+import java.time.LocalDateTime;
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class UserService {
+    private final UserRepository userRepository;
+    private final GoalService goalService;
+    private final EventService eventService;
+    private final MentorshipService mentorshipService;
 
-    public final UserRepository userRepository;
+    public void deactivateUser(long userId) {
+        User user = userRepository.findById(userId).orElseThrow(() ->
+                new IllegalArgumentException("Не удалось получить пользователя с id " + userId));
+
+        List<Goal> goals = user.getGoals();
+        goals.forEach(goal -> goalService.removeUserFromGoal(goal, userId));
+
+        LocalDateTime currentTime = LocalDateTime.now();
+        user.getOwnedEvents().forEach(event -> {
+            if (event.getStartDate().isAfter(currentTime)) { //Если ивент ещё не начался - удаляем
+                eventService.removeEvent(event.getId());
+            }
+        });
+
+
+        user.setActive(false);
+        userRepository.save(user);
+
+        mentorshipService.removeMentorship(userId);
+    }
 
     public User findById(long id) {
         return userRepository.findById(id)
