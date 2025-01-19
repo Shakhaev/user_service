ca34e50f6d6c5456845350674143c86c44542e4c
Roman Shevchuk <reacount1393@gmail.com>
2025-01-19 23:16:57 +0300

    Вроде задачу написал. Тесты напишу позже

--- src/main/java/school/faang/user_service/dto/UserDto.java
+++ src/main/java/school/faang/user_service/dto/UserDto.java
@@ -3,12 +3,9 @@ package school.faang.user_service.dto;
 import lombok.Builder;
 import lombok.Data;
 
-import java.util.List;
-
 @Data
 @Builder
 public class UserDto {
     private Long userId;
-    private List<MentorshipRequestDto> sentMentorshipRequests;
     private String username;
 }
--- src/main/java/school/faang/user_service/mapper/UserMapper.java
+++ src/main/java/school/faang/user_service/mapper/UserMapper.java
@@ -5,7 +5,7 @@ import org.mapstruct.ReportingPolicy;
 import school.faang.user_service.dto.UserDto;
 import school.faang.user_service.entity.User;
 
-@Mapper(componentModel = "spring", unmappedTargetPolicy = ReportingPolicy.IGNORE, uses = MentorshipRequestMapper.class)
+@Mapper(componentModel = "spring", unmappedTargetPolicy = ReportingPolicy.IGNORE)
 public interface UserMapper {
 
     UserDto toUserDto(User user);
