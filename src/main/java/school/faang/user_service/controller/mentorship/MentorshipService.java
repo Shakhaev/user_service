e729fbbe8597938d51d7f17235dcb1986a852bf3
Roman Shevchuk <reacount1393@gmail.com>
2025-01-19 23:09:01 +0300

    Вроде задачу написал. Тесты напишу позже

--- src/main/java/school/faang/user_service/controller/mentorship/MentorshipService.java
+++ src/main/java/school/faang/user_service/controller/mentorship/MentorshipService.java
@@ -12,6 +12,7 @@ import java.util.Optional;
 
 @Component
 public class MentorshipService {
+
     private final UserRepository userRepository;
 
     @Autowired
@@ -20,15 +21,61 @@ public class MentorshipService {
     }
 
     @NonNull
-    public List<User> getMentees(long userId){
-        List<User> userMentees = new ArrayList<User>();
-
+    public List<User> getMentees(long userId) {
         Optional<User> user = userRepository.findById(userId);
-        if(user.isEmpty()){
+        if (user.isEmpty()) {
             throw new RuntimeException("User not found");
         }
+
+        List<User> userMentees = new ArrayList<>();
         Optional<List<User>> mentees = Optional.ofNullable(user.get().getMentees());
         mentees.ifPresent(userMentees::addAll);
         return userMentees;
+    }
+
+    @NonNull
+    public List<User> getMentors(long userId) {
+
+
+        Optional<User> user = userRepository.findById(userId);
+        if (user.isEmpty()) {
+            throw new RuntimeException("User not found");
+        }
+
+        List<User> userMentors = new ArrayList<>();
+        Optional<List<User>> mentors = Optional.ofNullable(user.get().getMentors());
+        mentors.ifPresent(userMentors::addAll);
+        return userMentors;
+    }
+
+    private boolean mentorAndMenteeIsNotExists(long menteeId, long mentorId) {
+        Optional<User> mentee = userRepository.findById(menteeId);
+
+        if (mentee.isEmpty()) {
+            throw new RuntimeException("Mentor not found");
+        }
+
+        Optional<User> mentor = userRepository.findById(mentorId);
+        if (mentor.isEmpty()) {
+            throw new RuntimeException("Mentee not found");
+        }
+        return true;
+    }
+
+    public void deleteMentee(long menteeId, long mentorId) {
+        if (mentorAndMenteeIsNotExists(menteeId, mentorId)) {
+            throw new RuntimeException("Mentor and Mentee not found!");
+        }
+        Optional<User> mentor = userRepository.findById(mentorId);
+        mentor.get().getMentees().removeIf(menteeToRemove -> menteeToRemove.getId().equals(menteeId));
+    }
+
+    public void deleteMentor(long menteeId, long mentorId) {
+        if (mentorAndMenteeIsNotExists(menteeId, mentorId)) {
+            throw new RuntimeException("Mentor and Mentee not found!");
+        }
+        Optional<User> mentee = userRepository.findById(menteeId);
+        mentee.get().getMentors().removeIf(mentorToRemove -> mentorToRemove.getId().equals(mentorId));
+
     }
 }
