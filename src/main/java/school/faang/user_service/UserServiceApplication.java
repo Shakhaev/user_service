7484d96589301fd6926dd6cb5f9e57e7fbb0717d
Lexsandr <lexsandr@inbox.ru>
2025-01-19 16:45:03 +0300

    BJS2-55521 : 1) Removed EnvHelper class. Using @PropertySource annotation instead. 2) Removed all unnecessary methods. 3) Removed DotEnv library reference in build.gradle.kts.

--- src/main/java/school/faang/user_service/UserServiceApplication.java
+++ src/main/java/school/faang/user_service/UserServiceApplication.java
@@ -2,25 +2,20 @@ package school.faang.user_service;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
-import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
-import school.faang.user_service.config.EnvHelper;
+import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
 @EnableFeignClients("school.faang.user_service.client")
+@PropertySource("classpath:.env")
 public class UserServiceApplication {
 
     public static void main(String[] args)
     {
         SpringApplication.run(UserServiceApplication.class, args);
-    }
-
-    @PostConstruct
-    public void init() {
-        new EnvHelper().loadVariables();
     }
 
     @Bean
