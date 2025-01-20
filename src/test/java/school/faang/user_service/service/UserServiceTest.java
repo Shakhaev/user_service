9b6d509f52cfc83254222e3b2f8e98e25b6e3ed4
Mipusatina <mipspam@mail.ru>
2025-01-20 23:43:09 +0100

    Test: UserFilterByNamePatternTest

---
+++ src/test/java/school/faang/user_service/service/UserServiceTest.java
@@ -0,0 +1,17 @@
+package school.faang.user_service.service;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UserServiceTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void getPremiumUsers() {
+    }
+}
