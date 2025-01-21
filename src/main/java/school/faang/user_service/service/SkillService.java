f5bdddb5c0218ab16eeb3939d8be6b627391d867
Pavel Schelkin <rhanan1233@gmail.com>
2025-01-21 16:35:30 +0300

    Создание и настройка скиллов.

--- src/main/java/school/faang/user_service/service/SkillService.java
+++ src/main/java/school/faang/user_service/service/SkillService.java
@@ -1,13 +1,14 @@
 package school.faang.user_service.service;
 
+import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import school.faang.user_service.dto.skill.SkillCandidateDto;
 import school.faang.user_service.dto.skill.SkillDto;
 import school.faang.user_service.entity.Skill;
 import school.faang.user_service.entity.recommendation.SkillOffer;
 import school.faang.user_service.exception.DataValidationException;
-import jakarta.persistence.EntityNotFoundException;
 import school.faang.user_service.mapper.SkillMapper;
 import school.faang.user_service.repository.SkillRepository;
 import school.faang.user_service.repository.UserRepository;
@@ -21,8 +22,65 @@ public class SkillService {
     private final SkillRepository skillRepository;
     private final SkillMapper skillMapper;
     private final SkillOfferRepository skillOfferRepository;
-    private final UserRepository userRepository;
+    private final UserSkillGuaranteeRepository userSkillGuaranteeRepository;
+    
+    @Transactional
+    public SkillDto create(SkillDto skill) {
+        validateSkill(skill);
+        Skill skillToSave = skillMapper.toEntity(skill);
 
+        return skillMapper.toDto(skillRepository.save(skillToSave));
+
+    }
+
+    @Transactional
+    public List<SkillDto> getUserSkills(long userId) {
+        return skillRepository.findAllByUserId(userId).stream()
+                .map(skillMapper::toDto)
+                .toList();
+    }
+    @Transactional
+    public List<SkillCandidateDto> getOfferedSkills(long userId) {
+        return skillRepository.findSkillsOfferedToUser(userId).stream()
+                .map(skillMapper::toCandidateDto)
+                .toList();
+    }
+
+    @Transactional
+    public SkillDto acquireSkillFromOffers(long skillId, long userId) {
+        int MIN_SKILL_OFFERS = 3;
+        Skill skill = skillRepository.findById(skillId)
+                .orElseThrow(() -> new EntityNotFoundException("Skill with Id " + skillId + " not found!"));
+        Optional<Skill> userSkill = skillRepository.findUserSkill(skillId, userId);
+
+        if (userSkill.isPresent()) {
+            return null;
+        }
+        List<SkillOffer> allOffersOfSkill = skillOfferRepository.findAllOffersOfSkill(skillId, userId);
+        if (allOffersOfSkill.size() < MIN_SKILL_OFFERS) {
+            throw new IllegalArgumentException("This skill has been offered less than "
+                    + MIN_SKILL_OFFERS + " times.");
+        }
+        skillRepository.assignSkillToUser(skillId, userId);
+        for (SkillOffer offer : allOffersOfSkill) {
+            UserSkillGuarantee userSkillGuarantee = new UserSkillGuarantee();
+            userSkillGuarantee.setGuarantor(offer.getRecommendation().getAuthor());
+            userSkillGuarantee.setSkill(skill);
+            userSkillGuarantee.setUser(offer.getRecommendation().getReceiver());
+            userSkillGuaranteeRepository.save(userSkillGuarantee);
+        }
+
+        return skillMapper.toDto(skill);
+    }
+
+    private void validateSkill(SkillDto skillDto) {
+        if (skillDto.getTitle().isBlank()) {
+            throw new DataValidationException("The skill name cannot be empty!");
+        }
+        if (skillRepository.existsByTitle(skillDto.getTitle())) {
+            throw new DataValidationException("Skill with title " + skillDto.getTitle() + " already exist");
+        }
+    }
     public SkillDto findSkillById(Long id) {
         return skillMapper.toDto(skillRepository.findById(id)
                 .orElseThrow(() -> new EntityNotFoundException("Skill not found, id: " + id)));
@@ -36,48 +94,4 @@ public class SkillService {
                 .toList();
     }
 
-    public SkillDto create(SkillDto skill) {
-        validateSkill(skill);
-        Skill skillToSave = skillMapper.toEntity(skill);
-
-        return skillMapper.toDto(skillRepository.save(skillToSave));
-
-    }
-    public List<SkillDto> getUserSkills(long userId) {
-        return skillRepository.findAllByUserId(userId).stream()
-                .map(skillMapper::toDto)
-                .toList();
-    }
-
-    public List<SkillCandidateDto> getOfferedSkills(long userId) {
-        return skillRepository.findSkillsOfferedToUser(userId).stream()
-                .map(skillMapper::toCandidateDto)
-                .toList();
-    }
-
-    public SkillDto acquireSkillFromOffers(long skillId, long userId) {
-        int MIN_SKILL_OFFERS = 3;
-        if (skillRepository.findUserSkill(skillId, userId) != null) {
-            return null;
-        }
-        List<SkillOffer> allOffersOfSkill = skillOfferRepository.findAllOffersOfSkill(skillId, userId);
-        if (allOffersOfSkill.size() >= MIN_SKILL_OFFERS) {
-            skillRepository.assignSkillToUser(skillId, userId);
-        }
-        Skill skill = new Skill();
-        for (SkillOffer offer : allOffersOfSkill) {
-            skill.setGuarantees(offer.getSkill().getGuarantees());
-        }
-        skillRepository.save(skill);
-        return skillMapper.toDto(skill);
-    }
-
-    private void validateSkill(SkillDto skillDto) {
-        if (skillDto.getTitle().isBlank()) {
-            throw new DataValidationException("The skill name cannot be empty!");
-        }
-        if (skillRepository.existsByTitle(skillDto.getTitle())) {
-            throw new DataValidationException("Skill with title " + skillDto.getTitle() + " already exist");
-        }
-    }
 }
--- src/test/java/school/faang/user_service/service/SkillServiceTest.java
+++ src/test/java/school/faang/user_service/service/SkillServiceTest.java
@@ -1,5 +1,6 @@
 package school.faang.user_service.service;
 
+import jakarta.persistence.EntityNotFoundException;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mapstruct.factory.Mappers;
@@ -11,6 +12,8 @@ import org.mockito.Spy;
 import org.mockito.junit.jupiter.MockitoExtension;
 import school.faang.user_service.dto.skill.SkillDto;
 import school.faang.user_service.entity.Skill;
+import school.faang.user_service.entity.UserSkillGuarantee;
+import school.faang.user_service.entity.recommendation.SkillOffer;
 import school.faang.user_service.exception.DataValidationException;
 import school.faang.user_service.mapper.SkillMapper;
 import school.faang.user_service.repository.SkillRepository;
@@ -19,6 +22,8 @@ import school.faang.user_service.repository.recommendation.SkillOfferRepository;
 import java.util.List;
 import java.util.Optional;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.Mockito.times;
@@ -44,21 +49,21 @@ public class SkillServiceTest {
     private ArgumentCaptor<Skill> captor;
 
     @Test
-    public void testCreatingWithBlankNames() {
+    public void create_CreatingWithBlankNames() {
         SkillDto skillDto = new SkillDto(1L, " ");
 
         assertThrows(DataValidationException.class, () -> skillService.create(skillDto));
     }
 
     @Test
-    public void testCreatingWithExistTitle() {
+    public void create_CreatingWithExistTitle() {
         SkillDto skillDto = prepareData(true);
 
         assertThrows(DataValidationException.class, () -> skillService.create(skillDto));
     }
 
     @Test
-    public void testCreateSkill() {
+    public void create_CreateSkill() {
         SkillDto skillDto = prepareData(false);
 
         skillService.create(skillDto);
@@ -91,39 +96,91 @@ public class SkillServiceTest {
     }
 
     @Test
-    public void testAssignmentSkillWithoutRecommendedSkill() {
-        Skill skill = new Skill();
+    public void acquireSkillFromOffers_WithSkillNull() {
         long skillId = 1L;
         long userId = 1L;
         when(skillRepository.findUserSkill(skillId, userId)).thenReturn(null);
+
 
         Optional<Skill> userSkill = skillRepository.findUserSkill(skillId, userId);
 
         assertNull(userSkill);
     }
 
+    private final long SKILL_ID = 1L;
+    private final long USER_ID = 1L;
+
     @Test
-    public void  testAssignmentWithNoMinimumNumberOfRecommendations() {
+    public void acquireSkillFromOffers_SkillNotFound() {
 
-        int MIN_SKILL_OFFERS = 3;
-        long skillId = 1L;
-        long userId = 1L;
-        when(skillRepository.findUserSkill(1L, 1L)).thenReturn(null);
+        when(skillRepository.findById(SKILL_ID)).thenReturn(Optional.empty());
 
-        // Мокируем, что предложений скилла меньше минимального числа (например, 2 предложения)
-        when(skillOfferRepository.findAllOffersOfSkill(1L, 1L)).thenReturn(null);
+        assertThrows(EntityNotFoundException.class, () -> skillService.acquireSkillFromOffers(SKILL_ID, USER_ID));
 
-        // Выполняем метод
-        SkillDto result = skillService.acquireSkillFromOffers(1L, 1L);
-
-        // Проверяем, что результат равен null, т.к. предложений недостаточно
-        assertNull(result);
-
-        // Проверяем, что метод findAllOffersOfSkill был вызван
-        verify(skillOfferRepository, times(1)).findAllOffersOfSkill(1L, 1L);
-
+        verify(skillRepository, times(1)).findById(SKILL_ID);
 
     }
+
+    @Test
+    void testAcquireSkillFromOffers_UserAlreadyHasSkill() {
+        Skill skill = new Skill();
+        when(skillRepository.findById(SKILL_ID)).thenReturn(Optional.of(skill));
+        when(skillRepository.findUserSkill(SKILL_ID, USER_ID)).thenReturn(Optional.of(skill));
+
+        SkillDto skillDto = skillService.acquireSkillFromOffers(SKILL_ID, USER_ID);
+
+        assertNull(skillDto);
+        verify(skillRepository, times(1)).findUserSkill(SKILL_ID, USER_ID);
+    }
+
+    @Test
+    public void acquireSkillFromOffers_SuggestedLessThanTheStandardValue() {
+        Skill skill = new Skill();
+        when(skillRepository.findById(SKILL_ID)).thenReturn(Optional.of(skill));
+        when(skillRepository.findUserSkill(SKILL_ID, USER_ID)).thenReturn(null);
+        when(skillOfferRepository.findAllOffersOfSkill(SKILL_ID, USER_ID))
+                .thenReturn(List.of(new SkillOffer()));
+
+        assertThrows(IllegalArgumentException.class,
+                () -> skillService.acquireSkillFromOffers(SKILL_ID, USER_ID));
+    }
+
+    @Test
+    public void acquireSkillFromOffers_verifyAccept() {
+        Skill skill = new Skill();
+        skill.setId(SKILL_ID);
+        List<UserSkillGuarantee> userSkillGuarantees = List.of(new UserSkillGuarantee());
+
+        SkillOffer offer1 = new SkillOffer();
+        offer1.setSkill(skill);
+        offer1.getSkill().setGuarantees(userSkillGuarantees);
+
+        SkillOffer offer2 = new SkillOffer();
+        offer2.setSkill(skill);
+        offer2.getSkill().setGuarantees(userSkillGuarantees);
+
+        SkillOffer offer3 = new SkillOffer();
+        offer3.setSkill(skill);
+        offer3.getSkill().setGuarantees(userSkillGuarantees);
+
+        List<SkillOffer> offers = List.of(offer1, offer2, offer3);
+
+        when(skillRepository.findById(SKILL_ID)).thenReturn(Optional.of(skill));
+        when(skillRepository.findUserSkill(SKILL_ID, USER_ID)).thenReturn(null);
+        when(skillOfferRepository.findAllOffersOfSkill(SKILL_ID, USER_ID)).thenReturn(offers);
+        when(skillMapper.toDto(skill)).thenReturn(new SkillDto(SKILL_ID, "Test Skill"));
+
+        SkillDto result = skillService.acquireSkillFromOffers(SKILL_ID, USER_ID);
+
+        assertNotNull(result);
+        assertEquals(SKILL_ID, result.getId());
+        assertEquals("Test Skill", result.getTitle());
+
+        verify(skillRepository, times(1)).assignSkillToUser(SKILL_ID, USER_ID);
+        verify(skillRepository, times(offers.size())).save(skill);
+        verify(skillMapper, times(1)).toDto(skill);
+    }
+
 
     private SkillDto prepareData(boolean existTitle) {
         SkillDto skillDto = new SkillDto();
