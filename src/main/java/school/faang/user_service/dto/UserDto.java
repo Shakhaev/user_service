f8c523331343da29e435cbd5a319256b5a57e553
Roman Shevchuk <reacount1393@gmail.com>
2025-01-23 00:18:00 +0300

    Большую часть замечаний исправил

--- src/main/java/school/faang/user_service/dto/UserDto.java
+++ src/main/java/school/faang/user_service/dto/UserDto.java
@@ -3,9 +3,8 @@ package school.faang.user_service.dto;
 import lombok.Builder;
 import lombok.Data;
 
-@Data
 @Builder
-public class UserDto {
-    private Long userId;
-    private String username;
-}
+public record UserDto (
+    Long userId,
+    String username
+){}
