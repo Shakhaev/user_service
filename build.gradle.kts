50580a22d78f21b26b712af9c55ecac8e85c58a0
az3l1t <eternity_cr9p@mail.ru>
2025-01-23 20:08:43 +0300

    jacoco 2.2

--- build.gradle.kts
+++ build.gradle.kts
@@ -154,3 +154,24 @@ tasks {
         finalizedBy(jacocoCustomTestReport)
     }
 }
+
+checkstyle {
+    toolVersion = "10.17.0"
+    configFile = file("${project.rootDir}/config/checkstyle/checkstyle.xml")
+    checkstyle.enableExternalDtdLoad.set(true)
+}
+
+tasks.checkstyleMain {
+    source = fileTree("${project.rootDir}/src/main/java")
+    include("**/*.java")
+    exclude("**/resources/**")
+
+    classpath = files()
+}
+
+tasks.checkstyleTest {
+    source = fileTree("${project.rootDir}/src/test")
+    include("**/*.java")
+
+    classpath = files()
+}
