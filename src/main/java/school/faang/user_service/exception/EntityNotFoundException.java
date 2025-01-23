8c94c21c85778d7613bb6cd5a1a2f223ccc18ee1
shamshaev <an.shamshaev@gmail.com>
2025-01-24 02:44:09 +0300

    Change message to error

--- src/main/java/school/faang/user_service/exception/EntityNotFoundException.java
+++ src/main/java/school/faang/user_service/exception/EntityNotFoundException.java
@@ -1,7 +1,8 @@
 package school.faang.user_service.exception;
 
 public class EntityNotFoundException extends RuntimeException {
-    public EntityNotFoundException(String message) {
-        super(message);
+
+    public EntityNotFoundException(String error) {
+        super(error);
     }
 }
