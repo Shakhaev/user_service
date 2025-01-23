9620a5228d3ee2f35561f9c962ee15781523b89e
therealadik <vladerm2000@yandex.ru>
2025-01-23 18:10:31 +0500

    fix conflicts

--- src/main/java/school/faang/user_service/config/KafkaConstants.java
+++ src/main/java/school/faang/user_service/config/KafkaConstants.java
@@ -3,7 +3,7 @@ package school.faang.user_service.config;
 import lombok.experimental.UtilityClass;
 
 @UtilityClass
-public class KafkaTopics {
+public class KafkaConstants {
     public final static String PAYMENT_PROMOTION_TOPIC = "payment_promotion";
     public final static String EVENT_KEY = "event";
     public final static String USER_KEY = "user";
