a9f03037eedd7fbad31e063ccae7ad847aec1aa9
Maria Bortsova <bortsovamariia@gmail.com>
2025-01-23 15:57:59 +0300

    BJS2-52846: add swagger

--- src/main/java/school/faang/user_service/controller/EventController.java
+++ src/main/java/school/faang/user_service/controller/EventController.java
@@ -1,8 +1,11 @@
 package school.faang.user_service.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -17,6 +20,7 @@ import school.faang.user_service.service.EventService;
 
 import java.util.List;
 
+@Tag(name = "События")
 @Slf4j
 @RestController
 @RequiredArgsConstructor
@@ -24,36 +28,43 @@ import java.util.List;
 public class EventController {
     private final EventService eventService;
 
+    @Operation(summary = "Создать событие")
     @PostMapping
     public EventDto create(@RequestBody @Valid EventDto event) {
         return eventService.create(event);
     }
 
+    @Operation(summary = "Получить событие по идентификатору")
     @GetMapping("/{id}")
     public EventDto getEvent(@PathVariable("id") long id) {
         return eventService.getEvent(id);
     }
 
-    @PostMapping("/filter")
+    @Operation(summary = "Получить события с фильтрами")
+    @PostMapping(value = "/filter", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
     public List<EventDto> getEventsByFilter(@RequestBody EventFilterDto filter) {
         return eventService.getEventByFilters(filter);
     }
 
+    @Operation(summary = "Удалить событие")
     @DeleteMapping("/{id}")
     public void deleteEvent(@PathVariable long id) {
         eventService.deleteEvent(id);
     }
 
+    @Operation(summary = "Обновить событие")
     @PutMapping
     public EventDto updateEvent(@RequestBody EventDto event) {
         return eventService.updateEvent(event);
     }
 
+    @Operation(summary = "Получить события по идентификатору владельца")
     @GetMapping("/by-owned/{userId}")
     public List<EventDto> getOwnedEvents(@PathVariable("userId") long userId) {
         return eventService.getOwnedEvents(userId);
     }
 
+    @Operation(summary = "Получить события по идентификатору участника")
     @GetMapping("/by-owned-participated/{userId}")
     public List<EventDto> getParticipatedEvents(@PathVariable("userId") long userId) {
         return eventService.getParticipatedEvents(userId);
