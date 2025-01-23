ae6e080b0def53a463289cfa45d5b0772d07e80e
therealadik <vladerm2000@yandex.ru>
2025-01-23 14:56:49 +0500

    fix style

--- src/main/java/school/faang/user_service/config/SwaggerConfig.java
+++ src/main/java/school/faang/user_service/config/SwaggerConfig.java
@@ -8,7 +8,7 @@ import org.springframework.context.annotation.Configuration;
 @OpenAPIDefinition(info = @Info(
         title = "Микросервис пользователей",
         version = "1.0",
-        description = "Микросервис для работы с пользователями, их событиями, а также продвижением"
-))
+        description = "Микросервис для работы с пользователями, их событиями, а также продвижением")
+)
 public class SwaggerConfig {
 }
