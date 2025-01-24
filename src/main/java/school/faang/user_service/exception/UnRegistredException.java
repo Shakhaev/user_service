b9785db536713ab32c322f22de40b2cbc6f4c506
Artur Berdnikov <prikolovichp@inbox.ru>
2025-01-24 16:03:09 +0300

    Участвовать в Событии 1.Исправлены замечания по коду. 2.Написаны тесты

--- src/main/java/school/faang/user_service/exception/UnRegistredException.java
+++
@@ -1,7 +0,0 @@
-package school.faang.user_service.exception;
-
-public class UnRegistredException extends IllegalArgumentException {
-    public UnRegistredException(String message) {
-        super("Пользователь не ЗАРЕГИСТРИРОВАН на событие!");
-    }
-}
--- src/main/java/school/faang/user_service/exception/UserNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package school.faang.user_service.exception;
-
-public class UserNotFoundException extends IllegalArgumentException {
-    public UserNotFoundException(String message) {
-        super("Пользователь уже зарегистрирован!");
-    }
-}
