42748cbfb096303a2ff8ad6b91c5ef8b14aca19e
Mikhail Berkut <mikhail.tupitsyn@gmail.com>
2025-01-27 00:11:00 +0300

    fix: MentorshipServiceImpl added check if user exists

--- src/main/java/school/faang/user_service/service/mentorship/MentorshipServiceImpl.java
+++ src/main/java/school/faang/user_service/service/mentorship/MentorshipServiceImpl.java
@@ -1,5 +1,6 @@
 package school.faang.user_service.service.mentorship;
 
+import jakarta.persistence.EntityNotFoundException;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 import school.faang.user_service.dto.UserDto;
@@ -9,41 +10,50 @@ import school.faang.user_service.repository.UserRepository;
 import school.faang.user_service.repository.mentorship.MentorshipRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 @AllArgsConstructor
 public class MentorshipServiceImpl {
-    private final UserMapper userMapper;
 
-    private final MentorshipRepository mentorshipRepository;
+    private final UserMapper userMapper;
     private final UserRepository userRepository;
+    private final MentorshipRepository mentorshipRepository;
 
     public List<UserDto> getMentees(long userId) {
-        return userMapper.toDto(mentorshipRepository.findMenteesById(userId));
+        return Optional.ofNullable(userRepository.findById(userId))
+                .map(user -> userMapper.toDto(mentorshipRepository.findMenteesById(userId)))
+                .orElseThrow(() -> new EntityNotFoundException("Пользователь с id=" + userId + " не найден"));
     }
 
     public List<UserDto> getMentors(long userId) {
-        return userMapper.toDto(mentorshipRepository.findMentorsById(userId));
+        return Optional.ofNullable(userRepository.findById(userId))
+                .map(user -> userMapper.toDto(mentorshipRepository.findMentorsById(userId)))
+                .orElseThrow(() -> new EntityNotFoundException("Пользователь с id=" + userId + " не найден"));
     }
 
     public void deleteMentee(long menteeId, long mentorId) {
         User mentor = userRepository.getUserById(mentorId);
 
-        mentor.setMentees(
-                mentorshipRepository.findMenteesById(mentorId).stream()
-                        .filter(mentee -> mentee.getId() != menteeId)
-                        .toList()
-        );
+        if (mentor != null) {
+            mentor.setMentees(
+                    mentorshipRepository.findMenteesById(mentorId).stream()
+                            .filter(mentee -> mentee.getId() != menteeId)
+                            .toList()
+            );
+        }
     }
 
     public void deleteMentor(long menteeId, long mentorId) {
         User mentee = userRepository.getUserById(menteeId);
 
-        mentee.setMentors(
-                mentorshipRepository.findMentorsById(mentorId).stream()
-                        .filter(mentor -> mentor.getId() != mentorId)
-                        .toList()
-        );
+        if (mentee != null) {
+            mentee.setMentors(
+                    mentorshipRepository.findMentorsById(mentorId).stream()
+                            .filter(mentor -> mentor.getId() != mentorId)
+                            .toList()
+            );
+        }
     }
 
 }
