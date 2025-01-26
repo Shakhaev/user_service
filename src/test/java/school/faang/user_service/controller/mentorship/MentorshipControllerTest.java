a9c71875c6e9b29bc454153c2fb8a0abf27b3457
Roman Shevchuk <reacount1393@gmail.com>
2025-01-26 18:17:46 +0300

    Исправил тесты

--- src/test/java/school/faang/user_service/controller/mentorship/MentorshipControllerTest.java
+++ src/test/java/school/faang/user_service/controller/mentorship/MentorshipControllerTest.java
@@ -3,21 +3,30 @@ package school.faang.user_service.controller.mentorship;
 import org.junit.Assert;
 import org.junit.Test;
 import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import school.faang.user_service.exception.EntityNotFoundException;
+import school.faang.user_service.service.MentorshipServiceImpl;
 
 public class MentorshipControllerTest {
+
+    @Mock
+    private MentorshipServiceImpl mentorshipService;
 
     @InjectMocks
     private MentorshipController mentorshipController;
 
     @Test
-    public void testMentorshipController() {
+    public void testGetMenteesByIdSuccess() {
 
         //Тестируем проверки на null в методе getMentees
         Assert.assertThrows(
                 EntityNotFoundException.class,
                 () -> mentorshipController.getMentees(null)
         );
+    }
+
+    @Test
+    public void testGetMentorsByIdSuccess() {
 
         //Тестируем проверки на null в методе getMentors
         Assert.assertThrows(
@@ -25,13 +34,19 @@ public class MentorshipControllerTest {
                 () ->
                         mentorshipController.getMentors(null)
         );
+    }
 
+    @Test
+    public void testDeleteMenteeSuccess() {
         //Тестируем проверки на null в методе deleteMentee
         Assert.assertThrows(
                 EntityNotFoundException.class,
                 () -> mentorshipController.deleteMentee(null, null)
         );
+    }
 
+    @Test
+    public void testDeleteMentorSuccess() {
         //Тестируем проверки на null в методе deleteMentor
         Assert.assertThrows(
                 EntityNotFoundException.class,
