1c6419799ecd80eeea3b796f5882dd98d032fd53
az3l1t <eternity_cr9p@mail.ru>
2025-01-27 11:23:17 +0300

    fix 1.2

--- src/main/java/school/faang/user_service/controller/handler/ControllerExceptionHandler.java
+++ src/main/java/school/faang/user_service/controller/handler/ControllerExceptionHandler.java
@@ -8,26 +8,15 @@ import org.springframework.web.context.request.WebRequest;
 import school.faang.user_service.dto.error.ErrorResponse;
 import school.faang.user_service.exception.*;
 
-import java.time.LocalDateTime;
-
 @RestControllerAdvice
 public class ControllerExceptionHandler {
-
-    private ErrorResponse buildErrorMessage(Exception exception, WebRequest webRequest) {
-        String path = webRequest.getDescription(false).replace("uri=", "");
-        return ErrorResponse.builder()
-                .message(exception.getMessage())
-                .path(path)
-                .build();
-    }
-
-    @ExceptionHandler(value = UserWasNotFoundException.class)
+    @ExceptionHandler(value = {UserWasNotFoundException.class, ResourceNotFoundException.class, SkillNotFoundException.class})
     @ResponseStatus(value = HttpStatus.NOT_FOUND)
     public ErrorResponse userWasNotFound(UserWasNotFoundException exception, WebRequest webRequest) {
         return buildErrorMessage(exception, webRequest);
     }
 
-    @ExceptionHandler(value = DataValidationException.class)
+    @ExceptionHandler(value = {DataValidationException.class, UserGoalLimitExceededException.class})
     @ResponseStatus(value = HttpStatus.BAD_REQUEST)
     public ErrorResponse handleDataValidation(DataValidationException exception, WebRequest webRequest) {
         return buildErrorMessage(exception, webRequest);
@@ -39,39 +28,17 @@ public class ControllerExceptionHandler {
         return buildErrorMessage(exception, webRequest);
     }
 
-    @ExceptionHandler(value = RecommendationRequestCreatedException.class)
+    @ExceptionHandler(value = {RecommendationRequestCreatedException.class, RequestAlreadyProcessedException.class, UserAlreadyExistsException.class})
     @ResponseStatus(value = HttpStatus.CONFLICT)
     public ErrorResponse handleRecommendationRequestCreated(RecommendationRequestCreatedException exception, WebRequest webRequest) {
         return buildErrorMessage(exception, webRequest);
     }
 
-    @ExceptionHandler(value = RequestAlreadyProcessedException.class)
-    @ResponseStatus(value = HttpStatus.CONFLICT)
-    public ErrorResponse handleRequestAlreadyProcessed(RequestAlreadyProcessedException exception, WebRequest webRequest) {
-        return buildErrorMessage(exception, webRequest);
-    }
-
-    @ExceptionHandler(value = ResourceNotFoundException.class)
-    @ResponseStatus(value = HttpStatus.NOT_FOUND)
-    public ErrorResponse handleResourceNotFound(ResourceNotFoundException exception, WebRequest webRequest) {
-        return buildErrorMessage(exception, webRequest);
-    }
-
-    @ExceptionHandler(value = SkillNotFoundException.class)
-    @ResponseStatus(value = HttpStatus.NOT_FOUND)
-    public ErrorResponse handleSkillNotFound(SkillNotFoundException exception, WebRequest webRequest) {
-        return buildErrorMessage(exception, webRequest);
-    }
-
-    @ExceptionHandler(value = UserAlreadyExistsException.class)
-    @ResponseStatus(value = HttpStatus.CONFLICT)
-    public ErrorResponse handleUserAlreadyExists(UserAlreadyExistsException exception, WebRequest webRequest) {
-        return buildErrorMessage(exception, webRequest);
-    }
-
-    @ExceptionHandler(value = UserGoalLimitExceededException.class)
-    @ResponseStatus(value = HttpStatus.BAD_REQUEST)
-    public ErrorResponse handleUserGoalLimitExceeded(UserGoalLimitExceededException exception, WebRequest webRequest) {
-        return buildErrorMessage(exception, webRequest);
+    private ErrorResponse buildErrorMessage(Exception exception, WebRequest webRequest) {
+        String path = webRequest.getDescription(false).replace("uri=", "");
+        return ErrorResponse.builder()
+                .message(exception.getMessage())
+                .path(path)
+                .build();
     }
 }
