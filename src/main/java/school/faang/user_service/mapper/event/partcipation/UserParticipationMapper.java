7768b1e2b58ab9490bf79e2d94e90f626a9f7981
Alexey Rogoza <rgz.ttt@mail.ru>
2025-01-28 11:35:45 +0700

    changes after cross-review

--- src/main/java/school/faang/user_service/mapper/event/partcipation/UserParticipationMapper.java
+++ src/main/java/school/faang/user_service/mapper/event/partcipation/UserParticipationMapper.java
@@ -1,7 +1,6 @@
 package school.faang.user_service.mapper.event.partcipation;
 
 import org.mapstruct.Mapper;
-import org.mapstruct.Mapping;
 import org.mapstruct.ReportingPolicy;
 import school.faang.user_service.dto.event.participant.UserParticipationDto;
 import school.faang.user_service.entity.User;
