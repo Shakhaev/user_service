5623ab88671f49e53ac14174813101c39aac6217
Mikhail Berkut <mikhail.tupitsyn@gmail.com>
2025-01-29 21:00:02 +0300

    userMapper merge

--- src/main/java/school/faang/user_service/mapper/UserMapper.java
+++ src/main/java/school/faang/user_service/mapper/UserMapper.java
@@ -4,19 +4,15 @@ import org.mapstruct.Mapper;
 import org.mapstruct.NullValuePropertyMappingStrategy;
 import school.faang.user_service.dto.user.UserDto;
 import org.springframework.stereotype.Component;
-import school.faang.user_service.dto.UserDto;
 import school.faang.user_service.entity.User;
-
-@Mapper(componentModel = "spring",
-        nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
 import java.util.List;
 
 @Component
-@Mapper(componentModel = "spring")
+@Mapper(componentModel = "spring",
+        nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
 public interface UserMapper {
-    UserDto toDto(User user);
-}
 
+    UserDto toDto(User user);
     List<UserDto> toDto(List<User> users);
 
 }
