95a74d2cfc8f776b70e5b7d95e3d4adcccdcc65e
Natalya Vasileva <boryantheone228@yandex.ru>
2025-01-29 15:32:52 +0300

    merge

--- src/test/java/school/faang/user_service/service/RecommendationRequestServiceTest.java
+++ src/test/java/school/faang/user_service/service/RecommendationRequestServiceTest.java
@@ -1,12 +1,5 @@
 package school.faang.user_service.service;
 
-
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.ArgumentCaptor;
-import org.mockito.Captor;
-import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import school.faang.user_service.dto.RecommendationRequestDto;
@@ -34,7 +27,6 @@ import java.util.stream.Stream;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
