2708050a6943d4b6eb74182e9ccf2ce6aebdd741
Muhammadsolih Abdugafforov <salikhdev@gmail.com>
2025-01-29 17:28:36 +0500

    User premium filter success 2.0v

--- src/main/java/school/faang/user_service/dto/UserDto.java
+++ src/main/java/school/faang/user_service/dto/UserDto.java
@@ -2,8 +2,6 @@ package school.faang.user_service.dto;
 
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
-import lombok.Getter;
-import lombok.Setter;
 
 public record UserDto(@Positive @NotNull Long id, @NotNull String username, @NotNull String email) {
 }
