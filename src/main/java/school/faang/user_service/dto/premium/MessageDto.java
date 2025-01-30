9e0574c0a1f2cf0c1548a8fe59b6e28e73d98bdb
Muhammadsolih Abdugafforov <salikhdev@gmail.com>
2025-01-30 22:27:20 +0500

    Buy premium added. And connect with payment_service 2.4v

--- src/main/java/school/faang/user_service/dto/premium/MessageDto.java
+++
@@ -1,9 +0,0 @@
-package school.faang.user_service.dto.premium;
-
-import lombok.Builder;
-
-@Builder
-public record MessageDto(boolean status,
-                         String message,
-                         int code) {
-}
