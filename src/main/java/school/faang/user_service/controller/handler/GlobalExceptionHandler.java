3f21671825f76b51516e2d521ba1d220d8a64f59
Maria Bortsova <bortsovamariia@gmail.com>
2025-02-02 17:04:22 +0300

    BJS-52933: refactor exception handler

--- src/main/java/school/faang/user_service/controller/handler/GlobalExceptionHandler.java
+++ src/main/java/school/faang/user_service/controller/handler/GlobalExceptionHandler.java
@@ -1,31 +1,51 @@
 package school.faang.user_service.controller.handler;
 
+import feign.FeignException;
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import school.faang.user_service.dto.error.ErrorModel;
 import school.faang.user_service.exception.DataValidationException;
 import school.faang.user_service.exception.ResourceNotFoundException;
 
-@ControllerAdvice
+@RestControllerAdvice
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(DataValidationException.class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
-    public ResponseEntity<String> handleDataValidationException(DataValidationException ex) {
-        return new ResponseEntity<>(ex.getMessage(), HttpStatus.BAD_REQUEST);
+    public ErrorModel handleDataValidationException(DataValidationException ex) {
+        return createError(ex.getMessage());
     }
 
     @ExceptionHandler(ResourceNotFoundException.class)
     @ResponseStatus(HttpStatus.NOT_FOUND)
-    public ResponseEntity<String> handleResourceNotFoundException(ResourceNotFoundException ex) {
-        return new ResponseEntity<>(ex.getMessage(), HttpStatus.NOT_FOUND);
+    public ErrorModel handleResourceNotFoundException(ResourceNotFoundException ex) {
+        return createError(ex.getMessage());
+    }
+
+    @ExceptionHandler(EntityNotFoundException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public ErrorModel handleEntityNotFoundException(EntityNotFoundException ex) {
+        return createError(ex.getMessage());
+    }
+
+    @ExceptionHandler(FeignException.class)
+    @ResponseStatus(HttpStatus.BAD_GATEWAY)
+    public ErrorModel handleFeignException(FeignException ex) {
+        return createError(ex.getMessage());
     }
 
     @ExceptionHandler(Exception.class)
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
-    public ResponseEntity<String> handleGenericException(Exception ex) {
-        return new ResponseEntity<>("An unexpected error occurred", HttpStatus.INTERNAL_SERVER_ERROR);
+    public ErrorModel handleGenericException(Exception ex) {
+        return createError("An unexpected error occurred");
+    }
+
+    private ErrorModel createError(String message) {
+        return ErrorModel.builder()
+                .message(message)
+                .build();
     }
 }
---
+++ src/main/java/school/faang/user_service/dto/error/ErrorModel.java
@@ -0,0 +1,10 @@
+package school.faang.user_service.dto.error;
+
+import lombok.Builder;
+
+@Builder
+public class ErrorModel {
+
+    private String message;
+
+}
