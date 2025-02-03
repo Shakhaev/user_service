718e29a91e07dca9a92bdc197a1514671d6c4183
Dias Kamza <kamzadias31@gmail.com>
2025-02-03 11:49:53 +0500

    feature: added UserController, UserService and Tests (BJS2-53791)

--- src/test/java/school/faang/user_service/service/UserServiceTest.java
+++ src/test/java/school/faang/user_service/service/UserServiceTest.java
@@ -15,11 +15,10 @@ import school.faang.user_service.entity.event.Event;
 import school.faang.user_service.entity.goal.Goal;
 import school.faang.user_service.entity.premium.Premium;
 import school.faang.user_service.exception.ResourceNotFoundException;
-import school.faang.user_service.filters.interfaces.UserFilter;
-import school.faang.user_service.filters.subscription.CityFilter;
 import school.faang.user_service.mapper.UserMapperImpl;
 import school.faang.user_service.repository.UserRepository;
 import school.faang.user_service.service.event.EventService;
+import school.faang.user_service.service.filter.UserFilter;
 import school.faang.user_service.service.goal.GoalService;
 
 import java.time.LocalDateTime;
